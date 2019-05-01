42c85a61cd903e3d53d8a707e49901a03b81e216
Tawanda Kanyangarara <tawandakanyangarara@tawandas-mbp-2.home>
2019-05-01 18:30:33 +0100

    Megre Sort

---
+++ Algoyrythms/MergeSort.js
@@ -0,0 +1,35 @@
+function mergeSort(arr) {
+
+    if (arr.length < 2) {
+        return arr;
+    }
+
+    var middleIndex = Math.floor(arr.length / 2);
+    var firstHalf = arr.slice(0, middleIndex);
+    var secondHalf = arr.slice(middleIndex);
+
+    return merge(mergeSort(firstHalf), mergeSort(secondHalf));
+}
+
+function merge(array1, array2) {
+    var result = [];
+    while (array1.length && array2.length) {
+        var minElem;
+        if(array1[0] < array2[0]) {
+            minElem = array1.shift();
+        } else {
+            minElem = array2.shift();
+        }
+        result.push(minElem);
+    }
+
+    if(array1.length) {
+        result = result.concat(array1);
+    } else {
+        result = result.concat(array2);
+    }
+
+    return result;
+}
+
+console.log(mergeSort([6000,34,203,3,,764,200,984,198,764,1,9,1]));
